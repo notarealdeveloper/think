595eea5117ccafd1f754bbaa594c89aa6c95bee6
Jason Wilkes <notarealdeveloper@gmail.com>
2022-04-24 16:26:44 -0700

    get and set in thought space.

--- types.py
+++ types.py
@@ -29,9 +29,21 @@ class Object:
         self._check(object)
         self.object = object
         self.thought = Thought()
+        self.attrs = {}
 
     def think(self):
-        return self.thought.think()
+        """
+            This is not just self.thought.think().
+            This is a computation derived from that thought plus our top down knowledge.
+            That means it is the result of a computation that depends on learnables,
+            and we can take a gradient of it with respect to them.
+        """
+        t = self.thought.think()
+        t0 = t
+        for attr, value in self.attrs.items():
+            _pre_get_check_attr(attr)
+            t = _hardset(t, attr, value)
+        return t
 
     def rethink(self, t):
         return self.thought.rethink(t)
@@ -89,18 +101,95 @@ class Object:
     def __rtruediv__(self, other):
         return Div(other, self)
 
-    def __getitem__(self, key):
-        return Getitem(self, key)
+    def __eq__(self, other):
+        if self is other:
+            return True
+        if (self.object != other.object):
+            return False
+        a = self.thought.think()
+        b = other.thought.think()
+        return jnp.allclose(a, b).item()
 
-    def __setitem__(self, key, value):
-        return Setitem(self, key, value)
+    def get(self, Attr):
+        _pre_get_check_attr(Attr)
+        if Attr not in self.attrs:
+            feel = Attr.project(self)
+            print(f"get: {self} asked for {Attr}, but no explicit knowledge exists. "
+                  f"returning feeling: {feel}")
+            return feel
+        # here's where things get interesting
+        value = self.attrs[Attr]
 
-    def get(self, attr):
-        return Getattr(self, attr)
+        feel  = Attr.project(self)
+        know  = Attr.project(value)
 
-    def set(self, attr, value):
-        return Setattr(self, attr, value)
+        adeps = Attr.depends()
+        odeps = self.depends()
+        vdeps = value.depends()
 
+        print(f"Unsupervised learning time bitches!")
+        print(f"feel=know is the goal, and they're both computations with dependencies.")
+        print(f"Go.")
+        return {'feel':feel, 'know': know, 'odeps': odeps, 'vdeps': vdeps, 'adeps': adeps}
+
+    def set(self, Attr, value):
+        _pre_set_check_attr(Attr)
+        value = _pre_set_promote_value(Attr, value)
+        self._softset(Attr, value)
+        # TODO: REMEMBER THIS:
+        # If we do the hardset here (or if we ever do a hardset directly on a learnable),
+        # we don't "absorb" the knowledge. (i.e., we don't *understand how to produce it*.)
+        # instead, ONLY PERFORM HARDSETS ON THE OUTPUT OF COMPUTATIONS!
+        # i.e., on things that are NOT directly "settable"
+        # e.g., using the knowledge dictionary inside of get()
+        # self._hardset(Attr, value)
+        print(f"remember: set is being smart by not calling _hardset!")
+        return self
+
+    def softset(self, Attr, value):
+        _pre_set_check_attr(Attr)
+        value = _pre_set_promote_value(Attr, value)
+        self._softset(Attr, value)
+        return self
+
+    def _softset(self, Attr, value):
+        self.attrs[Attr] = value
+        return self
+
+    def __array__(self):
+        return self.think()
+
+
+### hard set implementation
+
+def hardset(object, Attr, value):
+    _pre_set_check_attr(Attr)
+    value = _pre_set_promote_value(Attr, value)
+    t_new = _hardset(object.think(), Attr, value)
+    return t_new
+
+def _hardset(t, Attr, value):
+    # we need Attr to still be fancy here!
+    t_in = Attr.project(t)
+    v_in = Attr.project(value)
+    t_out = t - t_in
+    t_new = v_in + t_out
+    return t_new
+
+def _pre_get_check_attr(Attr):
+    if not isinstance(Attr, Type):
+        raise TypeError(f"get: Attr {Attr} is not an instance of Type")
+
+def _pre_set_check_attr(Attr):
+    if not isinstance(Attr, Type):
+        raise TypeError(f"set: Attr {Attr} is not an instance of Type")
+
+def _pre_set_promote_value(Attr, value):
+    if not isinstance(value, Attr.object):
+        value = Attr(value)
+    return value
+
+### end hard set implementation
 
 class Str(Object):
     type = str
@@ -153,72 +242,91 @@ class Float(Number):
 class Type(Object):
     type = type
 
-    def __init__(self, name, base=object):
-        self.name    = name
-        self.base    = base
-        self.object  = type(name, (self.base,), {})
+    def __init__(cls, name, base=object):
+        cls.name    = name
+        cls.base    = base
+        cls.object  = type(name, (cls.base,), {})
+        cls.attrs   = {}
 
-    def __repr__(self):
-        return f"{self.name}"
+    def __call__(cls, object):
+        self = cls.object(object)
+        return self
 
+    def __repr__(cls):
+        return f"{cls.name}"
+
+    def __hash__(self):
+        return hash(self.object)
 
 # ================================
 
 
-class Integer(Type):
+class NumberType(Type):
+    def __init__(self, name, base=Number):
+        if not issubclass(base, Number):
+            raise TypeError(f"{name} has non-numeric base {base}")
+        Type.__init__(self, name, base)
+        self.origin = self.object.origin
+        self.vector = self.object.vector
+
+    def think(self, number=0):
+        number = self._unwrap(number)
+        s = self.object.sense(number)
+        b = self.object.origin.think()
+        w = self.object.vector.think()
+        return b + s*w
+
+    def project(self, object):
+        basis = [self.origin, self.vector]
+        return slow.project(basis, object)
+
+    def depends(self):
+        return [self.origin, self.vector]
+
+
+class Integer(NumberType):
     def __init__(self, name, base=Int):
-        if not issubclass(base, Number):
-            raise TypeError(f"{name} has non-numeric base {base}")
-        Type.__init__(self, name, base)
-        self.origin = self.object.origin
-        self.vector = self.object.vector
-
-    def __call__(self, int):
-        return self.object(int)
-
-    def think(self, int=0):
-        int = self._unwrap(int)
-        s = self.object.sense(int)
-        b = self.object.origin.think()
-        w = self.object.vector.think()
-        return b + s*w
+        super().__init__(name, base)
 
 
-class Floating(Type):
+class Floating(NumberType):
     def __init__(self, name, base=Float):
-        if not issubclass(base, Number):
-            raise TypeError(f"{name} has non-numeric base {base}")
-        Type.__init__(self, name, base)
-        self.origin = self.object.origin
-        self.vector = self.object.vector
-
-    def __call__(self, float):
-        return self.object(float)
-
-    def think(self, float=0.0):
-        float = self._unwrap(float)
-        s = self.object.sense(float)
-        b = self.object.origin.think()
-        w = self.object.vector.think()
-        return b + s*w
+        super().__init__(name, base)
 
 
-class String(Type):
-    def __init__(self, name, base=Str):
+class EnumType(Type):
+    def __init__(self, name, base):
         Type.__init__(self, name, base)
         self.thought = self.object.thought
         self.memory = {}
 
-    def __call__(self, str):
-        return self.memory.get(str) or self.memory.setdefault(str, self.object(str))
+    def __call__(self, object):
+        if object in self.memory:
+            return self.memory[object]
+        o = self.object(object)
+        self.memory[object] = o
+        return o
 
-    def think(self, str=None):
-        str = self._unwrap(str)
+    def think(self, object=None):
+        object = self._unwrap(object)
         T = self.thought.think()
-        if str is None:
+        if object is None:
             return T
-        t = self.memory[str].think()
+        t = self.memory[object].think()
         return slow.mix([T, t])
+
+    def project(self, object):
+        keys = list(self.memory.keys())
+        vals = list(self.memory.values())
+        return slow.project(vals, object)
+
+    def depends(self):
+        return [self.thought] + [o.thought for o in self.memory.values()]
+
+
+class String(EnumType):
+    def __init__(self, name, base=Str):
+        super().__init__(name, base)
 
 
 # ^ these guys are the ones who need invert methods!
@@ -422,4 +530,24 @@ if False:
         return softset(ts, t, v)
 
 
+Ticker  = String('Ticker')
+SPY     = Ticker('SPY')
+QQQ     = Ticker('QQQ')
+TLT     = Ticker('TLT')
 
+Age     = Integer('Age')
+Sector  = String('Sector')
+SEMI    = Sector('SEMI')
+TECH    = Sector('TECH')
+BANK    = Sector('BANK')
+
+SPY.set(Age, 42)
+assert SPY.attrs[Age] == Age(42)
+SPY.set(Age, Age(69))
+assert SPY.attrs[Age] == Age(69)
+
+Ticker.get(Age)
+# SPY.get(Age)
+
+SPY.set(Sector, SEMI)
+SPY.get(Sector)
