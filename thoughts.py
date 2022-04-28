f2836bf389b68a0e819b62bd1bdd6c4a69dcee5c
Jason Wilkes <notarealdeveloper@gmail.com>
2022-04-27 18:34:25 -0700

    Make THOUGHT_DIM module level, it was too complex before.

--- thoughts.py
+++ thoughts.py
@@ -2,19 +2,26 @@
 
 __all__ = [
     'Thought',
-    'new_thought_class',
+    'set_thought_dim',
 ]
 
 import jax.numpy as jnp
 
 from think.random import State
 
+STATE = State()
 
-class BaseThought:
+THOUGHT_DIM = 1024
 
-    STATE = State()
+def new_thought():
+    denominator = jnp.sqrt(THOUGHT_DIM)
+    return STATE.normal([THOUGHT_DIM])/denominator
 
-    THOUGHT_DIM = None
+def set_thought_dim(n):
+    global THOUGHT_DIM
+    THOUGHT_DIM = n
+
+class Thought:
 
     def __init__(self, t=None):
         self._t = t
@@ -22,7 +29,7 @@ class BaseThought:
     def think(self):
         # initialize lazily
         if self._t is None:
-            self._t = self._new_thought()
+            self._t = new_thought()
         return self._t
 
     def rethink(self, t):
@@ -33,20 +40,4 @@ class BaseThought:
 
     def __array__(self):
         return self._t
-
-    @classmethod
-    def _new_thought(cls):
-        denominator = jnp.sqrt(cls.THOUGHT_DIM)
-        return cls.STATE.normal([cls.THOUGHT_DIM])/denominator
-
-def new_thought_class(thought_dim, seed=42):
-
-    class Thought(BaseThought):
-        STATE = State(seed)
-        THOUGHT_DIM = thought_dim
-
-    return Thought
-
-
-Thought = new_thought_class(1024)
 
