2e037514cb75d8a323048eb85fdf192acc3550c6
Jason Wilkes <root@thedynamiclinker.com>
2025-12-16 19:24:23 -0600

    More mistakes

--- src/think/core.py
+++ src/think/core.py
@@ -143,26 +143,51 @@ class Type(type):
 
         return None
 
-    def __call__(cls, arg, *args, **kwds):
+    def __call__(cls, *args, **kwds):
+
+        if args:
+            arg = args[0]
+        else:
+            arg = None
 
         if hasattr(cls, '__object__'):
-            object = cls.__object__(arg, *args, **kwds)
+            object = cls.__object__(*args, **kwds)
         else:
             object = arg
+            hash = None # brilliant lol
 
-        try:    hash(object)
-        except: hashable = False
-        else:   hashable = True
+        try:
+            hash(object)
+            assert not args
+            assert not kwds
+        except:
+            hashable = False
+        else:
+            hashable = True
 
         if hashable:
             if object in cls.memory:
                 return cls.memory[object]
 
-        self = cls.__new__(cls, object, *args, **kwds)
+        t = kwds.pop('t', None)
 
+        if ALL_TYPES_NULLABLE:
+            if not isinstance(object, cls.object) and object is not None:
+                raise TypeError(f"object {object!r} is not of type {cls.object}")
+        else:
+            if not isinstance(object, cls.object):
+                raise TypeError(f"object {object!r} is not of type {cls.object}")
+
+        self = cls.__new__(cls, *args, **kwds)
+
+        self.object  = object
+        self.attrs   = {}
+        self.thought = Thought(t)
+        self.type    = type(cls)
         self.__raw__ = arg
+
         if isinstance(self, cls):
-            cls.__init__(self, object, *args, **kwds)
+            cls.__init__(self, *args, **kwds)
 
         if hashable:
             cls.memory[object] = self
@@ -285,26 +310,7 @@ class Object(metaclass=Type):
     object = object
 
     def __new__(cls, *args, **kwds):
-
-        if not args:
-            object = None
-        else:
-            object = args[0]
-
-        t = kwds.pop('t', None)
-
-        if ALL_TYPES_NULLABLE:
-            if not isinstance(object, cls.object) and object is not None:
-                raise TypeError(f"object {object!r} is not of type {cls.object}")
-        else:
-            if not isinstance(object, cls.object):
-                raise TypeError(f"object {object!r} is not of type {cls.object}")
-
         self = builtins.object.__new__(cls)
-        self.object  = object
-        self.attrs   = {}
-        self.thought = Thought(t)
-        self.type    = type(cls)
         return self
 
     def __init__(self, *args, **kwds):
